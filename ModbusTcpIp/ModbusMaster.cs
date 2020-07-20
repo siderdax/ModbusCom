e4f157e066803d08726d61f37cadebcd207f8fbb
yikim1 <yikim1@withtech.co.kr>
2020-07-20 10:21:43 +0900

    +Serial

---
+++ ModbusTcpIp/ModbusMaster.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO.Ports;
+using Modbus.Device;
+
+namespace ModbusTcpIp
+{
+    public class ModbusMaster : Modbus, IDisposable
+    {
+        private bool disposed = false;
+        private SerialPort _serial;
+        public SerialPort Serial { get => _serial; }
+
+        public string Port { get; private set; }
+        public int Baud { get; private set; }
+
+        private ModbusSerialMaster modbusSerialMaster;
+
+        /// <summary>
+        /// TCP Master
+        /// </summary>
+        public void StartMaster()
+        {
+            _serial = new SerialPort(Port, Baud);
+            _serial.Open();
+        }
+
+        public void StartMaster(string port, int baud)
+        {
+            _serial = new SerialPort(port, baud);
+            _serial.Open();
+        }
+
+        public void StopMaster()
+        {
+            _serial.Close();
+        }
+
+        public void StartSerialMaster()
+        {
+            modbusSerialMaster = ModbusSerialMaster.CreateRtu(_serial);
+        }
+
+        public void StopSerialMaster()
+        {
+            modbusSerialMaster.Dispose();
+        }
+
+        public ushort[] ReadSerialMasterHoldingRegisters(byte slaveId, ushort address, ushort length)
+        {
+            return modbusSerialMaster.ReadHoldingRegisters(slaveId, (ushort)(address - 1), length);
+        }
+
+        public void WriteSerialMasterHoldingRegister(byte slaveId, ushort address, ushort register)
+        {
+            modbusSerialMaster.WriteSingleRegister(slaveId, (ushort)(address - 1), register);
+        }
+
+        public ushort[] ReadSerialMasterInputRegisters(byte slaveId, ushort address, ushort length)
+        {
+            return modbusSerialMaster.ReadInputRegisters(slaveId, (ushort)(address - 1), length);
+        }
+
+        public ModbusMaster()
+        {
+            Port = String.Empty;
+            Baud = 9600;
+        }
+
+        public ModbusMaster(string port, int baud) : this()
+        {
+            Port = port;
+            Baud = baud;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _serial.Dispose();
+                    StopSerialMaster();
+                }
+                disposed = true;
+            }
+        }
+    }
+}
---
+++ ModbusTcpIp/ModbusSlave.cs
@@ -0,0 +1,119 @@
+﻿using System.Diagnostics;
+using System;
+using System.IO.Ports;
+using System.Threading;
+using System.Threading.Tasks;
+using Modbus.Data;
+using Modbus.Device;
+
+namespace ModbusTcpIp
+{
+    public class ModbusSlave : Modbus
+    {
+        private bool disposed = false;
+        private SerialPort _serial;
+        public SerialPort Serial { get => _serial; }
+
+        public string Port { get; private set; }
+        public int Baud { get; private set; }
+
+        private ModbusSerialSlave[] modbusSerialSlaves = new ModbusSerialSlave[MAX_SLAVE_ID];
+
+        /// <summary>
+        /// TCP Slave
+        /// </summary>
+        public void StartSlave()
+        {
+            _serial = new SerialPort(Port, Baud);
+            _serial.Open();
+        }
+
+        public void StartSlave(string port, int baud)
+        {
+            _serial = new SerialPort(port, baud);
+            _serial.Open();
+        }
+
+        public void StopSlave()
+        {
+            _serial.Close();
+        }
+
+        public void StartSerialSlave(byte slaveId)
+        {
+            ThrowRangeException(slaveId);
+            modbusSerialSlaves[slaveId - 1] = ModbusSerialSlave.CreateRtu(slaveId, _serial);
+            modbusSerialSlaves[slaveId - 1].DataStore = DataStoreFactory.CreateDefaultDataStore();
+            Task.Run(() => modbusSerialSlaves[slaveId - 1].Listen());
+        }
+
+        public void StopSerialSlave(byte slaveId)
+        {
+            ThrowRangeException(slaveId);
+            if (modbusSerialSlaves[slaveId - 1] != null)
+            {
+                modbusSerialSlaves[slaveId - 1].Dispose();
+            }
+        }
+
+        public void ClearSerialSlave()
+        {
+            foreach (ModbusSerialSlave s in modbusSerialSlaves)
+            {
+                s.Dispose();
+            }
+        }
+
+        public ushort ReadSerialSlaveHoldingRegister(byte slaveId, ushort address)
+        {
+            return modbusSerialSlaves[slaveId - 1].DataStore.HoldingRegisters[address];
+        }
+
+        public void WriteSerialSlaveHoldingRegister(byte slaveId, ushort address, ushort data)
+        {
+            modbusSerialSlaves[slaveId - 1].DataStore.HoldingRegisters[address] = data;
+        }
+
+        public ushort ReadSerialSlaveInputRegister(byte slaveId, ushort address)
+        {
+            return modbusSerialSlaves[slaveId - 1].DataStore.InputRegisters[address];
+        }
+
+        public void WriteSerialSlaveInputRegister(byte slaveId, ushort address, ushort data)
+        {
+            modbusSerialSlaves[slaveId - 1].DataStore.InputRegisters[address] = data;
+        }
+
+        public ModbusSlave()
+        {
+            Port = string.Empty;
+            Baud = 9600;
+        }
+
+        public ModbusSlave(string port, int baud) : this()
+        {
+            Port = port;
+            Baud = baud;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _serial.Close();
+                    _serial.Dispose();
+                    ClearSerialSlave();
+                }
+                disposed = true;
+            }
+        }
+    }
+}
